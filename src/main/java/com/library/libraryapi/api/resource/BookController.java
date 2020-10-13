a6ab485a7fc1299a7096fd275e2e45c758d0ab5c
JessicaRodriguesdc <jessicarodridc@gmail.com>
2020-10-13 16:58:43 -0300

    correcao

--- src/main/java/com/library/libraryapi/api/resource/BookController.java
+++ src/main/java/com/library/libraryapi/api/resource/BookController.java
@@ -24,7 +24,6 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 @RestController
 @RequestMapping("/api/books")
 @RequiredArgsConstructor
--- src/test/java/com/library/libraryapi/LibraryApiApplicationTests.java
+++ src/test/java/com/library/libraryapi/LibraryApiApplicationTests.java
@@ -9,5 +9,4 @@ class LibraryApiApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
