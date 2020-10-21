d66a08009b9fd9cb7ea7ad8fa5901fe2eca18df0
JessicaRodriguesdc <jessicarodridc@gmail.com>
2020-10-21 20:34:16 -0300

    correcoes

--- src/main/java/com/library/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/library/libraryapi/LibraryApiApplication.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
-public class LibraryApiApplication {
+public class LibraryApiApplication extends SpringBootServletInitializer {
 
 //	@Autowired
 //	private EmailService emailService;
